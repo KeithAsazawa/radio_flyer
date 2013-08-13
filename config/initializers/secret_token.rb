5fbaf331ab37ed0a2b728a5b9ea6819633f5f164
KeithAsazawa <keith.asazawa@gmail.com>
2013-08-13 15:59:42 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RadioFlyer::Application.config.secret_token = '55de4a1f70f8e1613f96952b5328f62971ff600d764cc84cbc9a8ba6c4ef794a63b6e9ed1a64aebd5cb161bdf0e769baa51e3052399e0257fd91626c39ed1720'
