38a53b4396ee128c4c86168ad8eebbd723c903e4
KeithAsazawa <keith.asazawa@gmail.com>
2013-08-20 19:01:55 -0700

    Add jPlayer to index page

---
+++ app/assets/javascripts/jplayer.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+  $("#jquery_jplayer_1").jPlayer({
+      swfPath: "/js/jplayer",
+      supplied: "mp3"
+    });
+  $('a.play').click(function(e) {
+      e.preventDefault();
+      var title = $(this).attr('alt'); 
+      $('#songtitle').html(title); 
+      $("#jquery_jplayer_1")
+          .jPlayer("setMedia", { mp3: this.href })
+          .jPlayer("play");
+  });
+});
